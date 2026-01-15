027733fffb50ca84d20d32664a64a5dd24bb667f
Debanjan Sinha Mahapatra <130504914+kakashihatake45@users.noreply.github.com>
2026-01-15 20:26:05 +0530

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,17 @@
-// Future: blog and notes loader
+
+async function loadMarkdown(file, targetId) {
+  const res = await fetch(file);
+  const text = await res.text();
+
+  const html = marked.parse(text);
+  const container = document.getElementById(targetId);
+
+  container.innerHTML = html;
+
+  renderMathInElement(container, {
+    delimiters: [
+      { left: "$$", right: "$$", display: true },
+      { left: "$", right: "$", display: false }
+    ]
+  });
+}
